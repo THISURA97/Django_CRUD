4ef0c15892f0ef39d35be54637ac8fbd0a84b278
THISURA97 <38825184+THISURA97@users.noreply.github.com>
2020-06-19 20:11:39 +0530

    heroku minor fix

--- newdj/settings.py
+++ newdj/settings.py
@@ -12,13 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +23,7 @@ STATICFILES_DIRS = (
 SECRET_KEY = '!@x7haf)c2cvr+yw-m9jo^5!9f3+k$zx*_p8(kvu%q)3e@tnkp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False,
 
 ALLOWED_HOSTS = []
 
@@ -129,6 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
