99dffc46faf3a04ccfc317b14f7ff16c99e52a3c
THISURA97 <38825184+THISURA97@users.noreply.github.com>
2020-06-20 19:06:25 +0530

    empty view path update

--- newdj/urls.py
+++ newdj/urls.py
@@ -20,7 +20,8 @@ from crud import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('emp', views.emp),  
-    path('show',views.show),  
+    path('show',views.show),
+    path('',views.show),  
     path('edit/<int:id>', views.edit),  
     path('update/<int:id>', views.update),  
     path('delete/<int:id>', views.destroy),  
