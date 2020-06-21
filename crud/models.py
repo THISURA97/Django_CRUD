ec707067dadff3f8ed0f6b4e51d0635a2f363d64
THISURA97 <38825184+THISURA97@users.noreply.github.com>
2020-06-21 10:29:57 +0530

    minor fix in fields

--- crud/models.py
+++ crud/models.py
@@ -6,6 +6,9 @@ class Employee(models.Model):
     eid = models.CharField(max_length=20)  
     ename = models.CharField(max_length=100)  
     eemail = models.EmailField()  
-    econtact = models.CharField(max_length=15)  
+    econtact = models.CharField(max_length=15)
+    date = models.DateField('%m/%d/%Y')
+    created_at = models.DateTimeField('%m/%d/%Y %H:%M:%S')
+    updated_at = models.DateTimeField('%m/%d/%Y %H:%M:%S')  
     class Meta:  
         db_table = "employee"  
--- crud/views.py
+++ crud/views.py
@@ -16,12 +16,15 @@ def emp(request):
     else:  
         form = EmployeeForm()  
     return render(request,'index.html',{'form':form})  
+
 def show(request):  
     employees = Employee.objects.all()  
     return render(request,"show.html",{'employees':employees})     
+
 def edit(request, id):  
     employee = Employee.objects.get(id=id)  
     return render(request,'edit.html', {'employee':employee})  
+
 def update(request, id):  
     employee = Employee.objects.get(id=id)  
     form = EmployeeForm(request.POST, instance = employee)  
@@ -29,6 +32,7 @@ def update(request, id):
         form.save()  
         return redirect("/show")  
     return render(request, 'edit.html', {'employee': employee})  
+
 def destroy(request, id):  
     employee = Employee.objects.get(id=id)  
     employee.delete()  
